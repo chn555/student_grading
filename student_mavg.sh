be60008d0893022e0918f691450fcf32bb3f58da
chn555 <chn555@gmail.com>
2018-02-07 18:46:02 +0200

    fixed add-grade

--- student_mavg.sh
+++ student_mavg.sh
@@ -10,8 +10,12 @@ for f in $files; do
   maxcount=${#sarr[*]}
   echo $maxcount
   tmpmavg=0
-  for i in {2..$maxcount}; do
-    tmpmavg=$(expr $tmpmavg + ${sarr[$i]})
+  if [[ $maxcount -le 1 ]]
+  then
+    continue
+  elif [[ $maxcount -eq 2 ]]
+  then
+    tmpmavg=${sarr[2]}
     if [[ $tmpmavg -gt $mavrg ]]
     then
       mavrg=$tmpmavg
@@ -19,7 +23,19 @@ for f in $files; do
     else
       continue
     fi
-  done
+  else
+    for i in $(eval echo {2..$( expr $maxcount - 1 )}); do
+      tmpmavg=$(expr $tmpmavg + ${sarr[$i]})
+    done
+    tmpmavg=$(expr $tmpmavg / $(expr $maxcount - 2))
+    if [[ $tmpmavg -gt $mavrg ]]
+    then
+      mavrg=$tmpmavg
+      mavrgstudent=$f
+    else
+      continue
+    fi
+  fi
 done
 
 echo "The best student was $mavrgstudent with $mavrg points avarage"
