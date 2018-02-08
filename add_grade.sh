2f9253a41037091c4f6e22b4980479b49be0bd6a
chn555 <chn566@gmail.com>
2018-02-08 22:35:23 +0200

    y

--- add_grade.sh
+++ add_grade.sh
@@ -1,9 +1,10 @@
 add_grade()
 {
-read -p "Enter student ID : " id
+
+#read -p "Enter student ID : " id
 # opens the file into an array called sarr
 # read -a sarr < $id
-read -a  sarr < $id.student
+read -a  sarr < "$id"
 
 # printts the array
 echo ${sarr[*]}
@@ -12,9 +13,42 @@ read -p "Enter grade to be added : " grade
 sarr+=("$grade")
 echo ${#sarr[*]}
 echo ${sarr[*]}
-echo ${sarr[*]} > $id.student
+echo ${sarr[*]} > $id
 
 echo "Grade $grade added "
 }
 
-add_grade
+menu()
+{
+
+  files=(*.student)
+  declare -a names
+
+  for f in $files; do
+    names+=$f
+  done
+
+
+    select opt in "${names[@]}" "Stop the script"; do
+    case $opt in
+      *.student)
+        echo "War file $opt selected"
+        id=$ops
+        echo $ops
+        add_grade
+        # processing
+        ;;
+      "Stop the script")
+        echo "You chose to stop"
+        break
+        ;;
+      *)
+        echo "This is not a number"
+        ;;
+    esac
+  done
+
+
+}
+
+menu
