493d8108822dda901103d318d6a8195e93db259d
chn <chn566@gmail.com>
2018-02-10 17:44:33 +0200

    sd

--- menu.sh
+++ menu.sh
@@ -12,24 +12,31 @@ select func in "Add a student" "Delete a student" "Add a grade to an existing st
   	 case $func in
 		"Add a student" | "add_student")
       add_student
+      exit 0
            ;;
 		"Delete a student" | "remove_student")
       remove_student
+      exit 0
 		       ;;
 		"Add a grade to an existing student" | "addgrade")
       add_grade
+      exit 0
 	         ;;
 		"Show avarage of a student" | "avgstudent")
       student_avg
+      exit 0
 		       ;;
 		"Show the student with the highest avarage" | "mavgstudent")
       student_mavg
+      exit 0
 		       ;;
 		"Replace the grades of two students with each other" | "replacegrade")
       replace_student
+      exit 0
 		       ;;
 		*)
 			echo "Please enter a valid selection"
 		;;
-	esac
+	esac2
+
 done
