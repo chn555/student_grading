272aa5db70a199f26231f56c80a63d8e77f731d7
chn <chn566@gmail.com>
2018-02-10 18:04:59 +0200

    fuckayou

--- menu.sh
+++ menu.sh
@@ -14,26 +14,38 @@ select func in "Add a student" "Delete a student" "Add a grade to an existing st
   	 case $func in
 		"Add a student" | "add_student")
       add_student
+      printf \n
+      printf \n
       mainmenu
            ;;
 		"Delete a student" | "remove_student")
       remove_student
+      printf \n
+      printf \n
       mainmenu
 		       ;;
 		"Add a grade to an existing student" | "addgrade")
       add_grade
+      printf \n
+      printf \n
       mainmenu
 	         ;;
 		"Show avarage of a student" | "avgstudent")
       student_avg
+      printf \n
+      printf \n
       mainmenu
 		       ;;
 		"Show the student with the highest avarage" | "mavgstudent")
       student_mavg
+      printf \n
+      printf \n
       mainmenu
 		       ;;
 		"Replace the grades of two students with each other" | "replacegrade")
       replace_student
+      printf \n
+      printf \n
       mainmenu
 		       ;;
     "Quit" | "q")
