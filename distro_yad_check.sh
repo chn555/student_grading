fe1ba3c11d1d68e24494dd7339f5a3f89b315899
tom <tom@arch>
2018-02-17 19:44:21 +0200

    distro_yad_check complete

--- distro_yad_check.sh
+++ distro_yad_check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-Manjaro_Distribution_Check ()
+Arch_Distribution_Check ()
 {
-	cat /etc/*-release |grep ID |cut -d "=" -f "2" |grep ^manjaro$
+	cat /etc/*-release |grep ID |cut -d "=" -f "2" |grep -e ^manjaro$ -e ^arch$ &> /dev/null
 	if [[ $? -eq 0 ]] ;then
-		Distro_Validation="manjaro"
+		Distro_Validation="arch"
 	else
 	   :
 	fi
@@ -13,7 +13,7 @@ Manjaro_Distribution_Check ()
 
 Debian_Distribution_Check ()
 {
-	cat /etc/*-release |grep ID |cut -d "=" -f "2" |grep ^debian$
+	cat /etc/*-release |grep ID |cut -d "=" -f "2" |grep ^debian$ &> /dev/null
 	if [[ $? -eq 0	]] ;then
 		Distro_Validation="debian"
 	else
@@ -23,7 +23,7 @@ Debian_Distribution_Check ()
 
 RedHat_Distribution_Check ()
 {
-	cat /etc/*-release |grep ID |cut -d "=" -f "2" |grep -e ^\"centos\"$ -e ^\"fedora\"$
+	cat /etc/*-release |grep ID |cut -d "=" -f "2" |grep -e ^\"centos\"$ -e ^\"fedora\"$ &> /dev/null
 	if [[ $? -eq 0	]] ;then
 		Distro_Validation="redhat"
 	else
@@ -31,11 +31,44 @@ RedHat_Distribution_Check ()
 	fi
 }
 
-yad_validation (){
+yad_validation ()
+{
   which yad &> /dev/null
   if [[ $? -eq 0 ]] ;then
     :
   else
-		if [[ $Debian_Distribution_Check =~ "manjaro"]]
+		if [[ $Distro_Validation =~ "arch" ]] ;then
+			sudo pacman -S yad --noconfirm &> /dev/null
+			if [[ $? -eq 0 ]] ;then
+				echo "Yad installation complete"
+			else
+				echo "Something went wrong..."
+				exit
+			fi
+		elif [[ $Distro_Validation =~ "debian" ]] ;then
+				sudo apt-get install yad -y &> /dev/null
+				if [[ $? -eq 0 ]] ;then
+					echo "Yad installation complete"
+				else
+					echo "Something went wrong..."
+					exit
+				fi
+		elif [[ $Distro_Validation =~ "redhat" ]] ;then
+				sudo yum install yad -y &> /dev/null
+				if [[ $? -eq 0 ]] ;then
+						echo "Yad installation complete"
+				else
+						echo "Something went wrong..."
+						exit
+				fi
+		else
+				echo "This script does not support your automatic installation of yad on your distribution"
+				echo "However you can install yad manualy and run again this script"
+				exit
+		fi
   fi
 }
+Arch_Distribution_Check
+Debian_Distribution_Check
+RedHat_Distribution_Check
+yad_validation
