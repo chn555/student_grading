6c49718ce5239825973861291099acb2c4a22b75
yahav <yahavperetz@gmail.com>
2018-02-24 18:17:58 +0200

    .

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 #Creator : fuck a you DorShamay && BigRush && Yahav && Chn555
 #Purpose : runs the scripts in function.sh
-#Version 0.0.1 12/02/18
+#Version 1 24/02/18
 
 source function.sh
 zenity_validation
