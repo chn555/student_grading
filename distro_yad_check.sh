24adc6dcb2b9c0b85ed3eb3a0a705c9241eb3854
tom <tom@localhost.localdomain>
2018-02-24 19:07:55 +0200

    zoomba working

--- distro_yad_check.sh
+++ distro_yad_check.sh
@@ -1,44 +1,69 @@
 #!/bin/bash
 
+<<COMMENT
+ This script will Check if zenity is installed on the computer, if not the script will install it according
+ to the installer that is available on the user's system.
+ If this script does not support the installer that is currently on the user's system,
+ the script will print to the user that he can install zenity manually and run the script again.
+COMMENT
 
-Arch_Distribution_Check (){
-	cat /etc/*-release |grep ID |cut -d "=" -f "2" |grep -e ^manjaro$ -e ^arch$ &> /dev/null
+## Checks if the user has pacman installer by running the "command" command on "pacman" and examining the exit code to check if the installer exists
+Pacman_Check (){
+	command pacman &> /dev/null
 	if [[ $? -eq 0 ]] ;then
-		Distro_Validation="arch"
+		Installer_Validation="PacManINST"
 	else
 	   :
 	fi
 }
 
-Debian_Distribution_Check (){
-	cat /etc/*-release |grep ID |cut -d "=" -f "2" |grep ^debian$ &> /dev/null
+## Checks if the user has apt-get installer by running the "command" command on "apt-get" and examining the exit code to check if the installer exists
+Apt-Get_Check (){
+	command apt-get &> /dev/null
 	if [[ $? -eq 0	]] ;then
-		Distro_Validation="debian"
+		Installer_Validation="AptGetINST"
 	else
 		:
 	fi
 }
 
-RedHat_Distribution_Check (){
-	cat /etc/*-release |grep ID |cut -d "=" -f "2" |grep -e ^\"centos\"$ -e ^\"fedora\"$ &> /dev/null
+## Checks if the user has yum installer by running the "command" command on "yum" and examining the exit code to check if the installer exists
+Yum_Check (){
+	command yum &> /dev/null
 	if [[ $? -eq 0	]] ;then
-		Distro_Validation="redhat"
+		Installer_Validation="YumINST"
 	else
 		:
 	fi
 }
 
+## Checks if the user has dnf installer by running the "command" command on "dnf" and examining the exit code to check if the installer exists
+Dnf_Check (){
+	command dnf &> /dev/null
+	if [[ $? -eq 0	]] ;then
+		Installer_Validation="DnfINST"
+	else
+		:
+	fi
+}
+
+<<COMMENT
+Checks if zentity is installed on the system and if not the script will install zenity based on the installer that is available,
+if the user is running on a distribution that does not have an installer that is supported by this script, it will print to the user that
+he can install zenity manually and run the script again.
+COMMENT
 zenity_validation (){
 	line=\#\#\#\#\#\#\#\#\#\#
-	Arch_Distribution_Check
-	Debian_Distribution_Check
-	RedHat_Distribution_Check
+	Pacman_Check
+	Apt-Get_Check
+	Yum_Check
+	Dnf_Check
   which zenity &> /dev/null
   if [[ $? -eq 0 ]] ;then
     :
   else
 		echo "zenity not installed"
-		if [[ $Distro_Validation =~ "arch" ]] ;then
+		if [[ $Installer_Validation =~ "PacManINST" ]] ;then
 			echo "$line Please enter password to install zenity $line"
 			sudo pacman -S zenity --noconfirm &> /dev/null
 			if [[ $? -eq 0 ]] ;then
@@ -47,7 +72,7 @@ zenity_validation (){
 				echo "$line Something went wrong $line"
 				exit
 			fi
-		elif [[ $Distro_Validation =~ "debian" ]] ;then
+		elif [[ $Installer_Validation =~ "AptGetINST" ]] ;then
 				echo "$line Please enter password to install zenity $line"
 				sudo apt-get install zenity -y &> /dev/null
 				if [[ $? -eq 0 ]] ;then
@@ -56,7 +81,16 @@ zenity_validation (){
 					echo "$line Something went wrong $line"
 					exit
 				fi
-		elif [[ $Distro_Validation =~ "redhat" ]] ;then
+		elif [[ $Installer_Validation =~ "YumINST" ]] ;then
+				echo "$line Please enter password to install zenity $line"
+				sudo yum install zenity -y &> /dev/null
+				if [[ $? -eq 0 ]] ;then
+						echo "$line zenity installation complete $line"
+				else
+						echo "$line Something went wrong $line"
+						exit
+				fi
+		elif [[ $Installer_Validation =~ "DnfINST" ]] ;then
 				echo "$line Please enter password to install zenity $line"
 				sudo yum install zenity -y &> /dev/null
 				if [[ $? -eq 0 ]] ;then
